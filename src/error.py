4165b01271f446d030979fee314a7d4ff566bd20
nickmahda (AWS) <nickmahda@gmail.com>
2018-10-13 18:58:40 +0000

    Finished fetcher class, updated handler

--- src/error.py
+++ src/error.py
@@ -1,26 +1,50 @@
+#import traceback
 import sys
 import os
-import traceback
 import config
+import signal
 
 def log(log_to=True):
     exc_type, exc_obj, exc_tb = sys.exc_info()
     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
     tb = f"{fname[1:-1]}: {exc_type.__name__} (line {exc_tb.tb_lineno}): \"{exc_obj}\""
     if log_to:
-        log=ope n(config.log,"w")
+        log=open(config.log,"w")
         log.write(tb)
         log.close()
     else:
         return tb
 
+class Handler:
+    def __init__(self):
+        self.killed = self.receivedTermSignal = False
+        catchSignals = [
+            1,
+            2,
+            3,
+            10,
+            12,
+            15,
+        ]
+        for signum in catchSignals:
+            signal.signal(signum, self.handler)
+
+    def handler(self, signum, frame):
+        self.lastSignal = signum
+        self.killed = True
+        if signum in [2, 3, 15]:
+            self.receivedTermSignal = True
+            
 class Logger:
+    def mark(self):
+        log(False)
+    def error(self):
+        log()
     def log(self, text):
         logfile = open(config.log, "w")
-        logfile.write(f"{text}")
-        logfile.close
+        logfile.write(f"{str(text)}")
+        logfile.close()
     def write(self, data):
         writefile = open(config.data, "w")
-        writefile.write(f"{data}")
+        writefile.write(f"{str(data)}")
         writefile.close()
-        
--- src/main.py
+++ src/main.py
@@ -1,18 +1,75 @@
+#!/usr/bin/env python3
 import re
 import time
 import logging
+import os
+import sys
 
-#from fetcher import Fetcher
+import praw
+
+import error
+from error import Handler, Logger
 import config
-from kill_handler import KillHandler
 
-logging.basicConfig(level=logging.DEBUG, filename=config.log, format=)
+r=praw.Reddit(client_id=config.client_id,
+              client_secret=config.client_secret,
+              password=config.password,
+              username=config.username,
+              user_agent=config.user_agent
+              )
+s=r.subreddit(config.subreddit)
+
+class Fetcher:
+    def __init__(self):
+        self.data = []
+        self.fetches = 0
+    def __call__(self, dry_run = False):
+        self.fetch(dry_run)    
+    def fetch(self, dry_run):
+        self.fetches += 1
+        for p in s.stream.submissions():
+            if not handler.killed:
+                print (f"Fetching new {self.fetches}")
+                try:
+                    self.data.append({})
+                    self.data[self.fetches - 1]["id"] = p
+                    self.data[self.fetches - 1]["title"] = p.title
+                    self.data[self.fetches - 1]["score"] = p.score
+                    self.data[self.fe
+                    if p.is_self:
+                        self.data[self.fetches - 1]["self"] = True
+                        self.data[self.fetches - 1]["selftext"] = p.selftext
+                    else:
+                        self.data[self.fetches - 1]["self"] = False
+                except Exception:
+                    logger.error()
+                    self.data[self.fetches - 1]["error"] = logger.mark()
+                finally:
+                    self.fetches+=1
+                    time.sleep(1)
+            else:
+                try:
+                    if not dry_run:
+                        logger.write(self.data)
+                    sys.exit("Received kill signal {}, exited gracefully".format(handler.lastSignal))
+                except Exception as e:
+                    print(error.log(False))
+                    sys.exit("An error occured when trying to exit gracefully, shutting down")
 
 def main():
-    killhandler=KillHandler()
-    while not killhandler.killed:
-        print(1)
-        time.sleep(0.5)
-
+    if "wipe" in sys.argv:
+        open(config.data, 'w').close()
+    global handler, logger, fetcher
+    handler=Handler()
+    logger=Logger()
+    fetcher=Fetcher()
+    if "dry" in sys.argv:
+        fetcher(True)
+    elif "none" not in sys.argv:
+        fetcher()
+    else:
+        sys.exit()
+        
+        
 if __name__ == "__main__":
     main()
